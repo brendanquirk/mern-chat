19273007737bc37af856268fb836ef531c16bdcc
brendanquirk <qui1217@gmail.com>
2024-06-04 13:17:17 -0400

    creating initial routes for sign up and login

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
 const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Testing base route')
 })
 
-app.listen(3001, () => {
-  console.log('server running on port 3001')
+app.get('/api/auth/signup', (req, res) => {
+  res.send('Sign Up Page')
+})
+
+app.get('/api/auth/login', (req, res) => {
+  res.send('Login Page')
+})
+
+app.listen(PORT, () => {
+  console.log(`Running on port: ${PORT}`)
 })
