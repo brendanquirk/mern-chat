350371a0c3c9e894589c9db7329e25aae3f6fbc6
brendanquirk <qui1217@gmail.com>
2024-06-05 11:18:55 -0400

    added extra security for jwt when not working on development

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,6 +9,7 @@ const generateTokenAndCreateCooke = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: 'strict',
+    secure: process.env.NODE_ENV !== 'development',
   })
 }
 
