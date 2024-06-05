b9b922bbdef4a551571d121aff241491f2d1bc98
brendanquirk <qui1217@gmail.com>
2024-06-05 11:01:39 -0400

    able to successfully create user with a hashed password stored in the database

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,41 @@
+import User from '../models/userModel.js'
+import bcrypt from 'bcryptjs'
+
 export const registerUser = async (req, res) => {
   try {
-    const { username, password, picture } = req.body
+    const { username, password, confirmPassword, picture } = req.body
+
+    //check if passwords match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'passwords do not match' })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    //see if user exists in DB already
+    const user = await User.findOne({ username })
+    if (user) {
+      return res.status(400).json({ error: 'username already exists' })
+    }
+
+    //create user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      picture:
+        picture === ''
+          ? 'https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png'
+          : picture,
+    })
+
+    await newUser.save()
+    return res.status(201).json({
+      message: 'user created successfully',
+      username: newUser.username,
+      password: newUser.password,
+      picture: newUser.picture,
+    })
   } catch (error) {
     console.log(error)
   }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ import {
 
 const router = express.Router()
 
-router.get('/signup', registerUser)
+router.post('/signup', registerUser)
 
 router.get('/login', loginUser)
 
