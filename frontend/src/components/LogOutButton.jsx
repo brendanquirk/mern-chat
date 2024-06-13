d3464a4f39ed84ef1d1f19068fe7399c380cc631
brendanquirk <brendanquirkdev@gmail.com>
2024-06-13 12:17:53 -0400

    added logout functionality

--- frontend/src/components/LogOutButton.jsx
+++ frontend/src/components/LogOutButton.jsx
@@ -1,6 +1,13 @@
+import useLogout from '../hooks/useLogout'
+
 const LogoutButton = () => {
+  const { logout } = useLogout()
+
   return (
-    <button className="btn-sm bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 rounded">
+    <button
+      className="btn-sm bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 rounded"
+      onClick={logout}
+    >
       Logout
     </button>
   )
---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,20 @@
+import toast from 'react-hot-toast'
+import axios from 'axios'
+import { useAuthContext } from '../context/AuthContext'
+
+const useLogout = () => {
+  const { setLoggedInUser } = useAuthContext()
+
+  const logout = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/api/auth/logout')
+      localStorage.removeItem('loggedInUser')
+      setLoggedInUser(null)
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+  return { logout }
+}
+
+export default useLogout
